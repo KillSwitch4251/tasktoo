39377523739c2c46fecd53f93835a578872a05b6
Cameron Wicks <G20w0388@ict.ru.ac.za>
2023-04-18 12:53:11 +0200

    Modified program to output injason format

--- XMLReader.java
+++ XMLReader.java
@@ -1,65 +1,37 @@
-import java.io.File;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Scanner;
+import org.json.JSONObject;
 
-public class Main {
+public class XMLReader {
     public static void main(String[] args) {
         try {
-            File xmlFile = new File("employee.xml");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter the path of the XML file: ");
+            String filePath = scanner.nextLine();
 
-            // Create a DocumentBuilder and parse the XML file
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(xmlFile);
-
-            // Normalize the document to remove any extraneous white space
-            doc.getDocumentElement().normalize();
-
-            // Check if the user has specified which fields to output
-            if (args.length > 0) {
-                // Loop through the specified fields and output their values
-                for (String field : args) {
-                    switch (field) {
-                        case "id":
-                            System.out.println("ID: " + doc.getElementsByTagName("id").item(0).getTextContent());
-                            break;
-                        case "name":
-                            System.out.println("Name: " + doc.getElementsByTagName("name").item(0).getTextContent());
-                            break;
-                        case "postalZip":
-                            System.out.println("Postal Zip: " + doc.getElementsByTagName("postalZip").item(0).getTextContent());
-                            break;
-                        case "region":
-                            System.out.println("Region: " + doc.getElementsByTagName("region").item(0).getTextContent());
-                            break;
-                        case "country":
-                            System.out.println("Country: " + doc.getElementsByTagName("country").item(0).getTextContent());
-                            break;
-                        case "address":
-                            System.out.println("Address: " + doc.getElementsByTagName("address").item(0).getTextContent());
-                            break;
-                        case "list":
-                            System.out.println("List: " + doc.getElementsByTagName("list").item(0).getTextContent());
-                            break;
-                        default:
-                            System.out.println("Invalid field: " + field);
-                    }
-                }
-            } else {
-                // If no fields are specified, output all fields
-                System.out.println("ID: " + doc.getElementsByTagName("id").item(0).getTextContent());
-                System.out.println("Name: " + doc.getElementsByTagName("name").item(0).getTextContent());
-                System.out.println("Postal Zip: " + doc.getElementsByTagName("postalZip").item(0).getTextContent());
-                System.out.println("Region: " + doc.getElementsByTagName("region").item(0).getTextContent());
-                System.out.println("Country: " + doc.getElementsByTagName("country").item(0).getTextContent());
-                System.out.println("Address: " + doc.getElementsByTagName("address").item(0).getTextContent());
-                System.out.println("List: " + doc.getElementsByTagName("list").item(0).getTextContent());
+            BufferedReader reader = new BufferedReader(new FileReader(filePath));
+            StringBuilder xmlContent = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                xmlContent.append(line.trim());
             }
+            reader.close();
+
+            JSONObject jsonObj = new JSONObject(xmlContent.toString());
+
+            System.out.println("Enter the field(s) you want to extract (comma-separated): ");
+            String fieldsStr = scanner.nextLine();
+            String[] fields = fieldsStr.split(",");
+
+            JSONObject selectedFieldsObj = new JSONObject();
+            for (String field : fields) {
+                selectedFieldsObj.put(field.trim(), jsonObj.get(field.trim()));
+            }
+
+            System.out.println(selectedFieldsObj.toString(4));
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
